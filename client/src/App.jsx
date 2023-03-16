f2b50c66a8be5a2ec6e420af02b1dd2ef450fea8
Ben Hur <Bhur777@gmail.com>
2023-03-16 12:08:52 -0400

    finish header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-	return <div>TEST</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
